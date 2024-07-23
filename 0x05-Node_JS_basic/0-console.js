341acb77f8c249f501a215491dd1bbc715210bf0
DeeGemini <zodiactheegemini@gmail.com>
2024-07-24 00:02:18 +0200

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+// 0-console.js
+
+/**
+ * Prints the provided message to STDOUT.
+ * @param {string} message - message to be printed
+ */
+
+function displayMassage(message) {
+  console.log(message);
+}
+
+module.exports = displayMassage;
