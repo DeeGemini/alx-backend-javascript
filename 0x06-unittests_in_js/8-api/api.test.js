cecaf7bd3dc256f9e019ff023dbdb4539f045512
DeeGemini <zodiactheegemini@gmail.com>
2024-07-25 21:04:59 +0000

    Added tests for the new endpoint

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -20,3 +20,29 @@ describe('Index page', function() {
     });
   });
 });
+
+describe('Cart page', function() {
+  const url = 'http://localhost:7865/cart/';
+
+  it('should return status code 200 when :id is a number', function(done) {
+    request(url + '12', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return status code 404 when :id is not a number', function(done) {
+    request(url + 'hello', function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('should return status code 404 when :id is missing', function(done) {
+    request(url, function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
