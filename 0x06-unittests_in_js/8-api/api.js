91b148872d376395003611c6c7320c842b97a354
DeeGemini <zodiactheegemini@gmail.com>
2024-07-25 21:11:15 +0000

    Added two new endpoints-GET /available_payments and POST /login

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -13,6 +13,20 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${id}`);
 });
 
+app.get('/available_payments', (req, res) => {
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  res.send(`Welcome ${userName}`);
+});
+
 app.use((req, res) => {
   res.status(404).send('Not Found');
 });
