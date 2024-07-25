aaffe0b0fa90a2ff3167cf2a2f90d2b135751634
DeeGemini <zodiactheegemini@gmail.com>
2024-07-25 21:01:12 +0000

    Added a new endpoint-/cart/:id

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const { id } = req.params;
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
